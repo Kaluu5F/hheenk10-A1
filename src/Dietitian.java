97bcae376a63a1b033e251b3163048a3338d7819
Kaluu5F <hirushadilshanp@gmail.com>
2025-07-18 20:48:31 +0530

    Dietitian class details  is added

--- src/Dietitian.java
+++ src/Dietitian.java
@@ -1,2 +1,21 @@
 public class Dietitian extends HealthProfessional {
+    private String specialityArea;
+
+    public Dietitian() {
+        super();
+        this.specialityArea = "General Nutrition";
+    }
+
+    public Dietitian(int id, String name, String clinic, String specialityArea) {
+        super(id, name, clinic);
+        this.specialityArea = specialityArea;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("Type: Dietitian");
+        super.printDetails();
+        System.out.println("Speciality: " + specialityArea);
+    }
 }
+
